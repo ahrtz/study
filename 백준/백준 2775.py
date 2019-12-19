00ba346be71e24007c14e1f13f487060409971b1
ahrtz <ahrtzzinn@gmail.com>
2019-12-19 16:04:58 +0900

    몰라몰라잉

---
+++ 백준/백준 2775.py
@@ -0,0 +1,35 @@
+#tri = int(input())
+
+#for i in tri:
+floor = int(input())
+room = int(input())
+mem= []
+cnt = 0    
+
+for i in range(1,room+1):
+    mem.append(i)
+
+if floor ==1 :
+    for i in range(room+1):
+        cnt+=i
+    print(cnt)
+else:
+
+/////////
+T = int(input())
+
+for _ in range(T):
+
+    k = int(input())
+
+    n = int(input())
+
+    people = [ i for i in range(1, n+1)]
+
+    for __ in range(k):
+
+        for j in range(1,n):
+
+            people[j] += people[j-1]
+
+    print(people[-1])
