29ba34dbb80d993b65984cbbd78b46d2cb1c598d
ahrtz <ahrtzzinn@gmail.com>
2019-12-25 17:57:39 +0900

    소수찾기

---
+++ 백준/백준 1929.py
@@ -0,0 +1,16 @@
+start,end = list(map(int, input().split(' ')))
+def is_prime(num):
+    if(num <= 1):
+        return False
+    
+    i = 2
+    while i * i <= num:
+        if num % i == 0:
+            return False
+        
+        i += 1
+    return True
+
+for i in range(start, end + 1):
+    if(is_prime(i)):
+        print(i)
