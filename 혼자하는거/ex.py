85ab2f57dc1f72190242378569f9432d6897fc9a
ahrtz <ahrtzzinn@gmail.com>
2020-02-04 20:57:40 +0900

    집에와서 한개

--- 혼자하는거/ex.py
+++ 혼자하는거/ex.py
@@ -1,3 +1,23 @@
 import itertools
-a=[1,2,3,4,5,6,7,8,9]
-print(list(itertools.combinations(a,3)))
+def permute(arr):
+    result = [arr[:]]
+    c = [0] * len(arr)
+    i = 0
+    while i < len(arr):
+        if c[i] < i:
+            if i % 2 == 0:
+                arr[0], arr[i] = arr[i], arr[0]
+            else:
+                arr[c[i]], arr[i] = arr[i], arr[c[i]]
+            result.append(arr[:])
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+            i += 1
+    return result
+
+
+a=[1,2,3,4]
+print(list(itertools.permutations(a,4)))
+print(permute(a))
