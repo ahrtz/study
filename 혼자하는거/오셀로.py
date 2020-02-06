ba4255d3d39faa8aa520b3cf60047ecc55776a3a
ahrtz <ahrtzzinn@gmail.com>
2020-02-06 22:09:56 +0900

    오셀로

--- 혼자하는거/오셀로.py
+++ 혼자하는거/오셀로.py
@@ -44,14 +44,14 @@ for tc in range(T):
             new_col = ccol - 1
             if 0 <= new_col < N and pan[rrow][new_col]!=color and pan[rrow][new_col]!=0:
                 ccol = new_col
-            if 0 <= new_col < N and pan[rrow][new_col] == 0:
+            elif 0 <= new_col < N and pan[rrow][new_col] == 0:
                 break
-            if 0<= new_col < N and pan[rrow][new_col]==color:
+            elif 0<= new_col < N and pan[rrow][new_col]==color:
                 tmpcol=new_col
                 for tttt in range(tmpcol,col-1,1):
                     pan[rrow][tttt]=color
                 break
-            if new_col<0:
+            elif new_col<0:
                 break
         rrow = row - 1
         ccol = col - 1
@@ -62,9 +62,9 @@ for tc in range(T):
             
             if 0 <= new_row < N and pan[new_row][ccol]!=color and pan[new_row][ccol]!=0:
                 rrow = new_row
-            if 0 <= new_row < N and pan[new_row][ccol] == 0:
+            elif 0 <= new_row < N and pan[new_row][ccol] == 0:
                 break
-            if 0<= new_row < N and pan[new_row][ccol]==color:
+            elif 0<= new_row < N and pan[new_row][ccol]==color:
                 tmprow=new_row
                 for tttt in range(tmprow,row-1,1):
                     pan[tttt][ccol]=color
@@ -79,14 +79,14 @@ for tc in range(T):
             new_row = rrow + 1
             if 0 <= new_row < N and pan[new_row][ccol]!=color and pan[new_row][ccol]!=0:
                 rrow = new_row
-            if 0 <= new_row < N and pan[new_row][ccol] == 0:
+            elif 0 <= new_row < N and pan[new_row][ccol] == 0:
                 break
-            if 0<= new_row < N and pan[new_row][ccol]==color:
+            elif 0<= new_row < N and pan[new_row][ccol]==color:
                 tmprow=new_row
                 for tttt in range(row-1,tmprow,1):
                     pan[tttt][ccol]=color
                 break
-            if new_row>=N:
+            elif new_row>=N:
                 break
         rrow = row - 1
         ccol = col - 1
@@ -98,15 +98,15 @@ for tc in range(T):
             if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]!=color and pan[new_row][new_col]!=0:
                 ccol = new_col
                 rrow = new_row
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
                 break
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
                 tmpcol = new_col
                 tmprow = new_row
                 for tttt in range(0,tmpcol-ccol):
                     pan[rrow-tttt][ccol+tttt]=color
                 break
-            if new_row<0 or new_col>=N:
+            elif new_row<0 or new_col>=N:
                 break
         rrow = row - 1
         ccol = col - 1
@@ -117,15 +117,15 @@ for tc in range(T):
             if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]!=color and pan[new_row][new_col]!=0:
                 ccol = new_col
                 rrow = new_row
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
                 break
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
                 tmpcol = new_col
                 tmprow = new_row
                 for tttt in range(0,tmpcol-ccol):
                     pan[rrow+tttt][ccol+tttt]=color
                 break
-            if new_row>=N or new_col >=N:
+            elif new_row>=N or new_col >=N:
                 break
         rrow = row - 1
         ccol = col - 1
@@ -136,15 +136,15 @@ for tc in range(T):
             if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]!=color and pan[new_row][new_col]!=0:
                 ccol = new_col
                 rrow = new_row
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
                 break
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
                 tmpcol = new_col
                 tmprow = new_row
                 for tttt in range(0,ccol-tmpcol):
                     pan[rrow-tttt][ccol-tttt]=color
                 break
-            if new_row<0 or new_col<0:
+            elif new_row<0 or new_col<0:
                 break
         rrow = row - 1
         ccol = col - 1
@@ -155,15 +155,15 @@ for tc in range(T):
             if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]!=color and pan[new_row][new_col]!=0:
                 ccol = new_col
                 rrow = new_row
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col] == 0:
                 break
-            if 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
+            elif 0 <= new_col < N and 0 <= new_row < N and pan[new_row][new_col]==color:
                 tmpcol = new_col
                 tmprow = new_row
                 for tttt in range(0,ccol-tmpcol):
                     pan[rrow+tttt][ccol-tttt]=color
                 break
-            if new_row>=N or new_col<0:
+            elif new_row>=N or new_col<0:
                 break
     w_cnt=0
     b_cnt=0
