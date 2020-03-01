3682fe8ecd6965931700a41a8fd8eedfbcbc2f91
ahrtz <ahrtzzinn@gmail.com>
2020-03-02 00:30:10 +0900

    디저트카페 아이디어

--- 알고리즘 연습/방학/디저트카페.py
+++ 알고리즘 연습/방학/디저트카페.py
@@ -11,3 +11,5 @@ for tc in range(T):
             while True:
                 for i in range(4):
 #try except 쓰면 될것같은데 술먹엇으니 내일 
+# 방향을 픽스하고 돌리기 try except 로 조건 충족하면 돌리기 
+#visited 를 set 으로 만들거나 cnt 에 +1 하기 
