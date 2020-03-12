db7a862c41da6b5878dce590d29f82a09efc0adc
ahrtz <ahrtzzinn@gmail.com>
2020-03-13 00:00:14 +0900

    링크드리스트 암호 다시풀기

--- 알고리즘 연습/방학/swea강의/암호(링크드리스트)_내방식.py
+++ 알고리즘 연습/방학/swea강의/암호(링크드리스트)_내방식.py
@@ -2,4 +2,18 @@ T= int(input())
 for tc in range(T):
     N,M,K = map(int,input().split())
     num_list = list(map(int,input().split()))
+    tmp=0
     
+    for _ in range(K):
+        tmp += (M)
+        if tmp >len(num_list):
+            tmp -= len(num_list)
+            tmp_num=num_list[tmp-1]+num_list[tmp]
+            num_list.insert(tmp,tmp_num)
+        elif tmp == len(num_list):
+            tmp_num=num_list[tmp-1]+num_list[0]
+            num_list.insert(tmp+1,tmp_num)
+        else:
+            tmp_num=num_list[tmp-1]+num_list[tmp]
+            num_list.insert(tmp,tmp_num)
+    print("#{} {}".format(tc+1," ".join(repr(k) for k in num_list[::-1][:10])))
