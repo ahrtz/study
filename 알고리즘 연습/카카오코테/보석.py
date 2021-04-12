f1c8bfcaf61207ede6efe4cbd4da57c5c0ccaa4b
ahrtz <ahrtzzinn@gmail.com>
2021-04-12 22:42:17 +0900

    보석쇼핑 투포인터

--- 알고리즘 연습/카카오코테/보석.py
+++ 알고리즘 연습/카카오코테/보석.py
@@ -1,18 +1,25 @@
 gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
-
-
-start = 0
-end=0 
-gems_set=set(gems)
-strt_idx=int(1e10)
-end_idx=0
-while start<=end and end<len(gems)+1  :
-    print(gems[start:end])
-    if set(gems[start:end])==gems_set:
-        strt_idx=start
-        end_idx=end
-        break
-    else:
-        end+=1
-
-print(strt_idx,end_idx)
+def solution(gems): 
+    size = len(set(gems)) 
+    dic = {gems[0]:1} 
+    temp = [0, len(gems) - 1] #답이 될 수 있는 후보 
+    start , end = 0, 0 
+    while(start < len(gems) and end < len(gems)): 
+        if len(dic) == size: 
+            if end - start < temp[1] - temp[0]:
+                temp = [start, end] 
+                if dic[gems[start]] == 1: 
+                    del dic[gems[start]] 
+                else: 
+                    dic[gems[start]] -= 1 
+                start += 1 
+        else: 
+            end += 1 
+            if end == len(gems): 
+                break 
+            if gems[end] in dic.keys(): 
+                dic[gems[end]] += 1 
+            else: 
+                dic[gems[end]] = 1 
+    return [temp[0]+1, temp[1]+1]
+print(solution(gems))
